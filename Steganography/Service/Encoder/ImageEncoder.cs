addbbfb8596d8035b759c67dff184b1ecae3d0a3
Blast <kingprostyle@yandex.ru>
2023-12-12 18:06:20 +0300

    initial eof_method implementation

--- Steganography/Service/Encoder/ImageEncoder.cs
+++ Steganography/Service/Encoder/ImageEncoder.cs
@@ -1,4 +1,6 @@
-﻿using Steganography.Repository;
+﻿using System.Text;
+using Steganography.Repository;
+using Steganography.Service.Utils;
 
 namespace Steganography.Service.Encoder;
 
@@ -11,8 +13,33 @@ public class ImageEncoder(IRepository repository) : IImageEncoder
         throw new NotImplementedException();
     }
 
-    public void EncodePastEOFMarker()
+    /// <summary>
+    /// Writes the message bytes after the JPEG's end of file marker.
+    /// </summary>
+    /// <remarks> 
+    /// Message must be encoded in ASCII
+    /// </remarks>
+    /// <param name="byte_array"></param>
+    /// <param name="message"></param>
+    /// <returns>Modified byte array representing the modified image </returns>
+    public byte[] WritePastEOFMarker(byte[] byte_array, string message)
     {
-        
+        byte[] message_bytes = Encoding.ASCII.GetBytes(message);
+
+        // Do not trust the IDE0300 message
+        int? EOF_index = SequenceLocator.LocateSequence(byte_array, new byte[] {0xFF, 0xD9}) ?? throw new Exception("Broken JPEG: Couldn't find EOF marker");
+
+        Array.Resize(ref byte_array, byte_array.Length+message_bytes.Length);
+
+        // Appends message bytes past EOF marker.
+        // TODO check for failure points, fix if needed.
+        int j = 0;
+        for (int i = (int)(EOF_index + 2); i < byte_array.Length; i++)
+        {
+            byte_array[i] = message_bytes[j];
+            j++;
+        }
+
+        return byte_array;  
     }
 }
