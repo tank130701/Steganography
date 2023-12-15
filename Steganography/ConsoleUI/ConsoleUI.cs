66a50fe0c7eadf9e0b9d46415cad1da506be8080
Vlad Medveditskov <mrbear13@yandex.ru>
2023-12-15 18:44:18 +0300

    Selecting Algorithm logic implementation

--- Steganography/ConsoleUI/ConsoleUI.cs
+++ Steganography/ConsoleUI/ConsoleUI.cs
@@ -8,105 +8,159 @@ namespace Steganography.ConsoleUI
     {
         private readonly IImageEncoder _encoder = encoder;
         private readonly IImageDecoder _decoder = decoder;
-        
+        private string _selectedAlgorithm = "The encoding algorithm is not selected. Choose an algorithm.";
+        private string _selectedFilePath = "The file is not selected";
         public void Run()
         {
+            Info mainMenuInfo = null;
             ConsoleMenu mainMenu = new ConsoleMenu("Main Menu",
                 new List<string>
                 {
                     MainMenuButtons.EncodeMessage, 
                     MainMenuButtons.DecodeMessage,
                     MainMenuButtons.Exit
-                });
-            
-            ConsoleMenu encodeMenu = new ConsoleMenu("Encode Menu", new List<string>
+                }, ref mainMenuInfo);
+
+            var encodeInfo = new Info(ref _selectedAlgorithm, ref _selectedFilePath);
+            ConsoleMenu encodeMenu = new ConsoleMenu( $"Select Option",
+                new List<string>
                 {
                     EncodeMenuButtons.SelectImage,
                     EncodeMenuButtons.SelectAlgorithm,
                     EncodeMenuButtons.WriteMessage,
                     EncodeMenuButtons.EncodeMessage,
                     EncodeMenuButtons.BackToMainMenu
-                });
-            
-            ConsoleMenu decodeMenu = new ConsoleMenu("Encode Menu", new List<string>
+                }, ref encodeInfo);
+
+            var decodeInfo = new Info(ref _selectedAlgorithm, ref _selectedFilePath);
+            ConsoleMenu decodeMenu = new ConsoleMenu("Select Option",
+                new List<string>
                 {
                     DecodeMenuButtons.SelectImage,
                     DecodeMenuButtons.SelectAlgorithm,
                     DecodeMenuButtons.DecodeMessage,
                     DecodeMenuButtons.BackToMainMenu
-                });
+                }, ref decodeInfo);
             
-            
-            State state = State.MainMenu;
-            string button = "";
-            while (state != State.Exit)
-            {
-                switch (state)
+            Info algorithmsMenuInfo = null;
+            ConsoleMenu algorithmsMenu = new ConsoleMenu("Select Algorithm",
+                new List<string>
                 {
-                    case State.MainMenu:
+                    EncodeAlgorithmsButtons.Lsb,
+                    EncodeAlgorithmsButtons.AlphaChannel,
+                    EncodeAlgorithmsButtons.Metadata,
+                    EncodeAlgorithmsButtons.Palette,
+                    EncodeAlgorithmsButtons.Dct,
+                    EncodeAlgorithmsButtons.F5,
+                    EncodeAlgorithmsButtons.BackToMenu
+                },ref algorithmsMenuInfo);
+            
+            
+            MenuStates menuStates = MenuStates.MainMenu;
+            string button = "";
+            while (menuStates != MenuStates.Exit)
+            {
+                switch (menuStates)
+                {
+                    case MenuStates.MainMenu:
                         button = mainMenu.DisplayMenu();
                         break;
-                    case State.EncodeMenu:
+                    case MenuStates.EncodeMenu:
                         button = encodeMenu.DisplayMenu();
                         break;
-                    case State.DecodeMenu:
+                    case MenuStates.DecodeMenu:
                         button = decodeMenu.DisplayMenu();
                         break;
+                    case MenuStates.AlgorithmsMenu:
+                        button = algorithmsMenu.DisplayMenu();
+                        break;
                 }
-                HandleInput(button, ref state);
+                HandleInput(button, ref menuStates);
             }
         }
         
-        void HandleInput(string button, ref State currentState)
+        void HandleInput(string button, ref MenuStates currentMenuStates)
         {
-            switch (currentState)
+            switch (currentMenuStates)
             {
-                case State.MainMenu:
+                case MenuStates.MainMenu:
                     switch (button)
                     {
                         case MainMenuButtons.EncodeMessage:
-                            currentState = State.EncodeMenu;
+                            currentMenuStates = MenuStates.EncodeMenu;
                             break;
                         case MainMenuButtons.DecodeMessage:
-                            currentState = State.DecodeMenu;
+                            currentMenuStates = MenuStates.DecodeMenu;
                             break;
                         case MainMenuButtons.Exit:
-                            currentState = State.Exit;
+                            currentMenuStates = MenuStates.Exit;
                             break;
                     }
                     break;
-                case State.EncodeMenu:
+                case MenuStates.EncodeMenu:
                     switch (button)
                     {
                         case EncodeMenuButtons.SelectImage:
                             Console.WriteLine("Вы выбрали опцию SelectImage в EncodeMenu");
                             break;
                         case EncodeMenuButtons.SelectAlgorithm:
-                            Console.WriteLine("Вы выбрали опцию SelectAlgorithm в подменю EncodeMenu");
+                            currentMenuStates = MenuStates.AlgorithmsMenu;
                             break;
                         case EncodeMenuButtons.WriteMessage:
                             break;
                         case EncodeMenuButtons.EncodeMessage:
                             break;
                         case EncodeMenuButtons.BackToMainMenu:
-                            currentState = State.MainMenu;
+                            currentMenuStates = MenuStates.MainMenu;
                             break;
                     }
                     break;
-                case State.DecodeMenu:
+                case MenuStates.DecodeMenu:
                     switch (button)
                     {
                         case DecodeMenuButtons.SelectImage: 
                             Console.WriteLine("Вы выбрали опцию SelectImage в DecodeMenu");
                             break;
+                        case DecodeMenuButtons.SelectAlgorithm:
+                            currentMenuStates = MenuStates.AlgorithmsMenu;
+                            break;
                         case DecodeMenuButtons.DecodeMessage:
                             Console.WriteLine("Вы выбрали опцию DecodeMessage в DecodeMenu");
                             break;
-                        case DecodeMenuButtons.SelectAlgorithm:
-                            Console.WriteLine("Вы выбрали опцию SelectAlgorithm в DecodeMenu");
-                            break;
                         case DecodeMenuButtons.BackToMainMenu:
-                            currentState = State.MainMenu;
+                            currentMenuStates = MenuStates.MainMenu;
+                            break;
+                    }
+                    break;
+                case MenuStates.AlgorithmsMenu:
+                    switch (button)
+                    {
+                        case EncodeAlgorithmsButtons.Lsb:
+                            _selectedAlgorithm = "LSB";
+                            currentMenuStates = MenuStates.EncodeMenu;
+                            break;
+                        case EncodeAlgorithmsButtons.AlphaChannel:
+                            _selectedAlgorithm = "AlphaChannel";
+                            currentMenuStates = MenuStates.EncodeMenu;
+                            break;
+                        case EncodeAlgorithmsButtons.Palette:
+                            _selectedAlgorithm = "Palette";
+                            currentMenuStates = MenuStates.EncodeMenu;
+                            break;
+                        case EncodeAlgorithmsButtons.Metadata:
+                            _selectedAlgorithm = "Metadata";
+                            currentMenuStates = MenuStates.EncodeMenu;
+                            break;
+                        case EncodeAlgorithmsButtons.Dct:
+                            _selectedAlgorithm = "DCT";
+                            currentMenuStates = MenuStates.EncodeMenu;
+                            break;
+                        case EncodeAlgorithmsButtons.F5:
+                            _selectedAlgorithm = "F5";
+                            currentMenuStates = MenuStates.EncodeMenu;
+                            break;
+                        case EncodeAlgorithmsButtons.BackToMenu:
+                            currentMenuStates = MenuStates.EncodeMenu;
                             break;
                     }
                     break;
---
+++ Steganography/ConsoleUI/Info.cs
@@ -0,0 +1,13 @@
+﻿namespace Steganography.ConsoleUI;
+
+public class Info(ref string selectedAlgorithm, ref string selectedFilePath)
+{
+    private string _selectedAlgorithm { get; set; } = selectedAlgorithm;
+    private string _selectedFilePath { get; set; } = selectedFilePath;
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Selected Algorithm: {_selectedAlgorithm}");
+        Console.WriteLine($"Selected FilePath: {_selectedFilePath}");
+    }
+}
