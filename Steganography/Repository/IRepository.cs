06749cb4f8edf6038cee9f753de59d4e6ccef11a
Leonid <smolyanov.lm@gmail.com>
2023-12-17 00:55:02 +0300

    LoadImage and SaveImage methods

--- Steganography/Repository/IRepository.cs
+++ Steganography/Repository/IRepository.cs
@@ -5,14 +5,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Steganography.Repository
 {
     public interface IRepository
     {
-        Bitmap LoadImageToBitmap(string imagePath);
-        string SaveImageFromBitmap(Bitmap image);
-        byte[] LoadImageToBytes(string imagePath);
-        string SaveImageFromBytes(byte[] image);
-        List<string> GetImageFilesInDirectory(string directoryPath);
+        Task<byte[]> LoadImage(string path); // Принимает строку (путь) и возвращает массив байтов
+        Task<string> SaveImage(byte[] imageData); // Принимает массив байтов и возвращает строку (путь)
+    }
+
+    public class Repositoty2 : IRepository
+    {
+        public async Task<byte[]> LoadImage(string path)
+        {
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException("Файл не найден", path);
+            }
+
+            return await File.ReadAllBytesAsync(path);
+        }
+
+        public async Task<string> SaveImage(byte[] imageData)
+        {
+            if (imageData == null || imageData.Length == 0)
+            {
+                throw new ArgumentException("Данные изображения пусты");
+            }
+
+            string path = Path.Combine("path_to_save_directory", $"{Guid.NewGuid()}.png");
+            await File.WriteAllBytesAsync(path, imageData);
+
+            return path;
+        }
     }
 }
