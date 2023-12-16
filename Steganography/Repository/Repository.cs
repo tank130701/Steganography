2fd07bee047df298957d32c281bfbe9286266447
Leonid <smolyanov.lm@gmail.com>
2023-12-17 01:22:08 +0300

    Rework methods

--- Steganography/Repository/Repository.cs
+++ Steganography/Repository/Repository.cs
@@ -1,47 +1,99 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading.Tasks;
 
-namespace Steganography.Repository;
-
-public class Repository : IRepository
+namespace Steganography.Repository
 {
-    public Bitmap LoadImageToBitmap(string imagePath)
+    public class Repository : IRepository
     {
-        throw new NotImplementedException();
-    }
-
-    public string SaveImageFromBitmap(Bitmap image)
-    {
-        throw new NotImplementedException();
-    }
-
-    public byte[] LoadImageToBytes(string imagePath)
-    {
-        throw new NotImplementedException();
-    }
-
-    public string SaveImageFromBytes(byte[] image)
-    {
-        throw new NotImplementedException();
-    }
-
-    public List<string> GetImageFilesInDirectory(string directoryPath)
-    {
-        List<string> imageFileList = new List<string>();
-        const string root = "./";
-        try
+        public Bitmap LoadImageToBitmap(string imagePath)
         {
-            string[] imageExtensions = { ".jpg", ".jpeg", ".png", ".gif", ".bmp" };
-            foreach (string extension in imageExtensions)
+            if (!File.Exists(imagePath))
             {
-                string[] files = Directory.GetFiles(root + directoryPath, "*" + extension);
-                imageFileList.AddRange(files);
+                throw new FileNotFoundException("Файл не найден", imagePath);
             }
-        }
-        catch (IOException ex)
-        {
-            throw new IOException($"Error when getting the list of images: {ex.Message}", ex);
+
+            return new Bitmap(imagePath);
         }
 
-        return imageFileList;
+        public string SaveImageFromBitmap(Bitmap image)
+        {
+            if (image == null)
+            {
+                throw new ArgumentNullException(nameof(image), "Изображение не может быть null");
+            }
+
+            string path = Path.Combine("path_to_save_directory", $"{Guid.NewGuid()}.png");
+
+            // Сохранение в формате PNG, можно выбрать другой формат, если нужно
+            image.Save(path, ImageFormat.Png);
+
+            return path;
+        }
+
+        public byte[] LoadImageToBytes(string imagePath)
+        {
+              if (!File.Exists(imagePath))
+         {
+             throw new FileNotFoundException("Файл не найден", imagePath);
+         }
+
+         using (Image image = Image.FromFile(imagePath))
+         {
+             using (MemoryStream ms = new MemoryStream())
+             {
+                 // Сохранение изображения в MemoryStream
+                 image.Save(ms, image.RawFormat);
+
+                 return ms.ToArray();
+             }
+         }
+        }
+
+        public string SaveImageFromBytes(byte[] image)
+        {
+                if (image == null || image.Length == 0)
+                {
+                    throw new ArgumentException("Данные изображения пусты", nameof(image));
+                }
+
+                string path = Path.Combine("path_to_save_directory", $"{Guid.NewGuid()}.png");
+
+                using (MemoryStream ms = new MemoryStream(image))
+                {
+                    using (Image img = Image.FromStream(ms))
+                    {
+                        // Сохранение изображения
+                        img.Save(path, ImageFormat.Png);
+                    }
+                }
+
+                return path;
+            }
+        
+
+        public List<string> GetImageFilesInDirectory(string directoryPath)
+        {
+            List<string> imageFileList = new List<string>();
+            const string root = "./";
+            try
+            {
+                string[] imageExtensions = { ".jpg", ".jpeg", ".png", ".gif", ".bmp" };
+                foreach (string extension in imageExtensions)
+                {
+                    string[] files = Directory.GetFiles(root + directoryPath, "*" + extension);
+                    imageFileList.AddRange(files);
+                }
+            }
+            catch (IOException ex)
+            {
+                throw new IOException($"Error when getting the list of images: {ex.Message}", ex);
+            }
+
+            return imageFileList;
+        }
     }
 }
