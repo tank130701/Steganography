552e0f4e94b79fd4d6dcaf1a670c7710c73ef5d6
Vlad Medveditskov <mrbear13@yandex.ru>
2023-12-19 22:20:01 +0300

    Add 1 method to Decoder

--- Steganography/Service/Decoder/IImageDecoder.cs
+++ Steganography/Service/Decoder/IImageDecoder.cs
@@ -3,5 +3,6 @@ namespace Steganography.Service.Decoder
     public interface IImageDecoder
     {
         string DecodeMessage(string imagePath, string algorithm);
+        public List<string> GetImageFilesInDirectory();
     }
 }
