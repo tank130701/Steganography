d8efc35a1fd92adbb068faef736c66ab97df1059
Gosha <shchegl00v@yandex.ru>
2023-12-19 16:27:55 +0300

    Добавил цикл проходки по всем пикслеям, записывание в последний бит пикселя и условие выхода из цикла

--- Steganography/Service/Encoder/IImageEncoder.cs
+++ Steganography/Service/Encoder/IImageEncoder.cs
@@ -13,9 +13,30 @@ namespace Steganography.Service.Encoder
     public class ImageEncoder : IImageEncoder
         {
             public void EncodeText(string imagePath, string outputImagePath, string message, string algorithm)
-                    {
-                        Bitmap bmp = new Bitmap(imagePath);
-                        int messageIndex = 0;
+            {
+                Bitmap bmp = new Bitmap(imagePath);
+                int messageIndex = 0;
 
+            for (int i = 0; i < bmp.Height; i++)
+                {
+                    for (int j = 0; j < bmp.Width; j++)
+                    {
+                        if (messageIndex == message.Length) break;
+
+                        Color pixel = bmp.GetPixel(j, i);
+                        char character = message[messageIndex++];
+
+                        int value = character;
+                        int R = (value >> 16) & 255;
+                        int G = (value >> 8) & 255;
+                        int B = value & 255;
+
+                        Color newPixel = Color.FromArgb(R, G, B);
+                        bmp.SetPixel(j, i, newPixel);
+                    }
+
+                    if (messageIndex == message.Length) break;
+                }
+            }
         }
 }
