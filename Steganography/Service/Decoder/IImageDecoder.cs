53d10c4271bc1e354951f9b60e4200adb0f10d47
Gosha <shchegl00v@yandex.ru>
2023-12-19 16:14:38 +0300

    Почистил за собой

--- Steganography/Service/Decoder/IImageDecoder.cs
+++ Steganography/Service/Decoder/IImageDecoder.cs
@@ -1,7 +1,42 @@
-namespace Steganography.Service.Decoder
+using System;
+using System.Drawing; // Для работы с изображениями
+using System.Drawing.Imaging;
+
+namespace Steganography.Service
 {
-    public interface IImageDecoder
+    internal interface IImageDecoder
     {
-        string DecodeMessage(string imagePath, string algorithm);
+        void DecodeText(string imagePath);
+
+
+    internal class ImageDecoder : IImageDecoder
+    {
+        public void DecodeText(string imagePath)
+        {
+            Bitmap bmp = new Bitmap(imagePath);
+
+            StringBuilder decodedText = new StringBuilder();
+
+            Color lastPixel = bmp.GetPixel(bmp.Width - 1, bmp.Height - 1);
+            int messageLength = lastPixel.B; // Предположим, что длина сообщения хранится в синем канале последнего пикселя
+
+            for (int i = 0, colorIndex = 0; i < messageLength; i++)
+            {
+                Color pixel = bmp.GetPixel(colorIndex % bmp.Width, colorIndex / bmp.Width);
+                
+                // Извлекаем биты символа из каждого канала цвета
+                int value = pixel.R;
+                value = (value << 8) + pixel.G;
+                value = (value << 8) + pixel.B;
+
+                char c = (char)value; // Преобразуем значение в символ
+
+                decodedText.Append(c); // Добавляем символ к декодированному тексту
+
+                colorIndex++;
+            }
+
+            Console.WriteLine("Decoded Text: " + decodedText.ToString());
+        }
     }
 }
