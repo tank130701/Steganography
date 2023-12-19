8876c721aa346c152f38c86e9981027d0bb690cd
Gosha <shchegl00v@yandex.ru>
2023-12-19 16:57:50 +0300

    Добавил цикл проходки по альфа-каналам пикселя добавляющих в него информацию и сохранение изменений

--- Steganography/Service/Encoder/IImageEncoder.cs
+++ Steganography/Service/Encoder/IImageEncoder.cs
@@ -69,7 +69,20 @@ namespace Steganography.Service.Encoder
             // Сохраняем длину сообщения в альфа-канале первого пикселя
             Color firstPixel = bmp.GetPixel(0, 0);
             Color newFirstPixel = Color.FromArgb(message.Length, firstPixel.R, firstPixel.G, firstPixel.B);
-            bmp.SetPixel(0, 0, newFirstPixel
+            bmp.SetPixel(0, 0, newFirstPixel);
+            
+            for (int i = 1; i <= message.Length; i++)
+            {
+                int x = i % bmp.Width;
+                int y = i / bmp.Width;
+
+                Color pixel = bmp.GetPixel(x, y);
+                char character = message[messageIndex++];
+                Color newPixel = Color.FromArgb(character, pixel.R, pixel.G, pixel.B);
+                bmp.SetPixel(x, y, newPixel);
+            }
+            
+            bmp.Save(outputImagePath, ImageFormat.Png);
         }
     }
 }
