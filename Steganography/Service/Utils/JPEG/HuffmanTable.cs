56b1071489179b2dd9114fcb732125862fb0801d
Blast1FM <kingprostyle@yandex.ru>
2024-01-01 12:29:45 +0300

    scuffed huffman table struct

---
+++ Steganography/Service/Utils/JPEG/HuffmanTable.cs
@@ -0,0 +1,10 @@
+﻿namespace Steganography.Service.Utils;
+
+public struct HuffmanTable
+{
+    public HuffmanTable(){}
+    byte[] offsets = [0];
+    byte[] symbols = [0];
+    uint[] codes = [0];
+    bool set = false;
+}
