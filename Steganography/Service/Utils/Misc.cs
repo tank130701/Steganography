15b0ecf153e9a927dd912915e779fb239adfeb28
Blast <kingprostyle@yandex.ru>
2024-01-07 15:25:42 +0300

    nibbles

--- Steganography/Service/Utils/Misc.cs
+++ Steganography/Service/Utils/Misc.cs
@@ -11,5 +11,17 @@ public static class Misc
     public static bool GetBit(this byte b, int bitNumber)
     {
         return (b & (1 << bitNumber)) != 0;
-    }   
+    }
+
+    /// <summary>
+    /// Splits byte into 2 bytes, with values representing the 4 bit nibbles
+    /// </summary>
+    /// <param name="b"></param>
+    /// <returns>Tuple of upper and lower nibbles</returns>
+    public static (byte upperNibble, byte lowerNibble) SplitIntoNibbles(this byte b)
+    {
+        byte upperNibble = (byte)   ((b & 0xF0) >> 4);
+        byte lowerNibble = (byte)   (b & 0x0F);
+        return (upperNibble, lowerNibble);
+    }
 }
